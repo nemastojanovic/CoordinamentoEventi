68057b324611912fd8731bf48c4ae26f63e2d6bf
nemastojanovic <31541799+nemastojanovic@users.noreply.github.com>
2019-10-15 10:22:41 +0200

    15.10

---
+++ Database/database.sql
@@ -0,0 +1,39 @@
+create database progettoEventi;
+use progettoEventi; 
+
+-- Struttura della tabella `utente`
+CREATE TABLE `utente` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `nome` varchar(20) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `accesso` enum('Amministratore','Ente') DEFAULT NULL,
+  `password` binary(64) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Struttura della tabella `luogo`
+CREATE TABLE `luogo` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `CAP` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Struttura della tabella `evento`
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `titolo`varchar(20) NOT NULL,
+  `tipo`varchar(20) NOT NULL,
+  `orarioInizio`datetime NOT NULL,
+  `orarioFine`datetime,
+  `osservazioni`varchar(50),
+  `id_luogo` int(11) NOT NULL,
+  `id_utente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+  -- Le foreign key
+ALTER TABLE `evento`
+  ADD CONSTRAINT `fk_evento_utente` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_evento_luogo` FOREIGN KEY (`id_luogo`) REFERENCES `luogo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
